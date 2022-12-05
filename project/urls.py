967e07f21aec4c64f5b6b350543574aeb5db4017
Lucas San Cristobal <Lucassanc01@outlook.es>
2022-12-05 10:33:25 -0300

    Agrego Familiares(Clase 19)

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ejemplo.views import index, monstrar_familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludar/', index),
+    path('mi-familia/', monstrar_familiares),
 ]
